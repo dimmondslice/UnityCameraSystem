4ae6b0b1d93d0dcf9eb5a877c8cd24cf24d46c5d
Spencer <johnss18@rpi.edu>
2016-11-28 19:19:52 -0500

    detection of character entering screen boundries added

---
+++ ExperimentalCamera/Assets/CameraBehavior.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour
+{
+	public Transform mainTarget;
+
+	public float maxDist;
+	public Vector3 defaultOffset;
+
+	private Transform camTrans;
+	private Camera cam;
+	private Vector3 camLocalPosLastFrame;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cam = GetComponentInChildren<Camera>();
+		camTrans = cam.transform;
+
+		transform.position = mainTarget.TransformPoint(defaultOffset);
+		transform.LookAt(mainTarget);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//camTrans.localPosition = camLocalPosLastFrame;
+
+		//Vector3 ssp = cam.WorldToViewportPoint(mainTarget.position);
+		//print(ssp);
+		//if (ssp.x <= .2 || ssp.x >= .8f || ssp.y <= .4 || ssp.y >= .6)
+		//{
+		//	transform.LookAt(mainTarget);
+		//	transform.position = Vector3.Lerp( transform.position, mainTarget.TransformPoint(defaultOffset),.1f);
+		//}
+
+		//camTrans.localPosition = Vector3.Lerp(camLocalPosLastFrame, Vector3.zero, .05f);
+		//camLocalPosLastFrame = camTrans.localPosition;
+		if (DetectIfObjectOutsideScreenSpaceBoundry(mainTarget, .8f, .2f))
+		{
+			transform.LookAt(mainTarget);
+			GetComponent<Rigidbody>().velocity = mainTarget.GetComponent<Rigidbody>().velocity;
+		}
+	}
+
+	private bool DetectIfObjectOutsideScreenSpaceBoundry( Transform trackedObject, float xBoundry, float yBoundry )
+	{
+		Vector3 objInLocalSpace = trackedObject.position - camTrans.position;
+		Vector3 objInHorizontalViewPlane = Vector3.ProjectOnPlane(objInLocalSpace, camTrans.up);
+		Vector3 objInVerticalViewPlane = Vector3.ProjectOnPlane(objInLocalSpace, camTrans.right);
+		float xAngle = Vector3.Angle(transform.forward, objInHorizontalViewPlane);
+		float yAngle = Vector3.Angle(transform.forward, objInVerticalViewPlane);
+
+		float xRatio = xAngle / (cam.fieldOfView / 2);
+		float yRatio = yAngle / (cam.fieldOfView / 2);
+
+		//print("xRatio:" + xRatio + "     yRatio:" + yRatio);
+		//Debug.DrawRay(transform.position, transform.forward);
+		//Debug.DrawRay(transform.position, objInLocalSpace, Color.magenta);
+
+		if (xRatio > xBoundry)
+			return true;
+		else if (yRatio > yBoundry)
+			return true;
+		else return false;
+	}
+}
---
+++ ExperimentalCamera/Assets/SimpleMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour
+{
+	public float speed;
+	public float rotateSpeed;
+
+	public Transform moveRelativeToObj;
+
+	Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate ()
+	{
+		//Vector3 walkDir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized * speed;
+		//walkDir.y = rb.velocity.y;
+		//rb.velocity = transform.TransformDirection(walkDir);
+
+		//transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * rotateSpeed);
+
+		Vector3 inputVec = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+		Vector3 relativeSpaceInputVec = moveRelativeToObj.TransformDirection(inputVec);
+
+		relativeSpaceInputVec.y = 0f;
+		
+
+		if (inputVec != Vector3.zero)
+		{
+			transform.forward = Vector3.Lerp(transform.forward, relativeSpaceInputVec, .2f);
+		}
+
+		rb.velocity = relativeSpaceInputVec * speed;
+
+		//rb.velocity = transform.forward * relativeSpaceInputVec.magnitude * Mathf.Sign(Input.GetAxis("Vertical")) * speed;
+	}
+}
